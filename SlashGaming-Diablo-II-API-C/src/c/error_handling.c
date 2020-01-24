71a01ace4d2991c574c026f4b62eca21bb962f43
IAmTrial <IAmRealTrial@gmail.com>
2020-01-23 23:09:39 -0800

    Add error handling

---
+++ SlashGaming-Diablo-II-API-C/src/c/error_handling.c
@@ -0,0 +1,135 @@
+/**
+ * SlashGaming Diablo II Modding API for C
+ * Copyright (C) 2018-2020  Mir Drualga
+ *
+ * This file is part of SlashGaming Diablo II Modding API for C.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Additional permissions under GNU Affero General Public License version 3
+ *  section 7
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with Diablo II (or a modified version of that game and its
+ *  libraries), containing parts covered by the terms of Blizzard End User
+ *  License Agreement, the licensors of this Program grant you additional
+ *  permission to convey the resulting work. This additional permission is
+ *  also extended to any combination of expansions, mods, and remasters of
+ *  the game.
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with any Graphics Device Interface (GDI), DirectDraw, Direct3D,
+ *  Glide, OpenGL, or Rave wrapper (or modified versions of those
+ *  libraries), containing parts not covered by a compatible license, the
+ *  licensors of this Program grant you additional permission to convey the
+ *  resulting work.
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with any library (or a modified version of that library) that links
+ *  to Diablo II (or a modified version of that game and its libraries),
+ *  containing parts not covered by a compatible license, the licensors of
+ *  this Program grant you additional permission to convey the resulting
+ *  work.
+ */
+
+#include "error_handling.h"
+
+#include <windows.h>
+#include <wchar.h>
+
+static const wchar_t* kFunctionFailErrorFormat =
+    L"File: %s \n"
+    L"Line: %d \n"
+    L"\n"
+    L"The function %s failed with error code %X.";
+
+static const wchar_t* kGeneralFailErrorFormat =
+    L"File: %s \n"
+    L"Line: %d \n"
+    L"\n"
+    L"%s";
+
+void ExitOnGeneralFailure(
+    const wchar_t* message,
+    const wchar_t* caption,
+    const wchar_t* file_name,
+    int line
+) {
+#if _DEBUG
+  wchar_t full_message[512];
+
+  swprintf(
+      full_message,
+      sizeof(full_message) / sizeof(full_message[0]),
+      kGeneralFailErrorFormat,
+      file_name,
+      line,
+      message
+  );
+
+  MessageBoxW(
+      NULL,
+      full_message,
+      caption,
+      MB_OK | MB_ICONERROR
+  );
+#endif // _DEBUG
+
+  exit(EXIT_FAILURE);
+}
+
+void ExitOnAllocationFailure(
+    const wchar_t* file_name,
+    int line
+) {
+  ExitOnGeneralFailure(
+      L"Allocation function failed.",
+      L"Memory Allocation Failed",
+      file_name,
+      line
+  );
+}
+
+void ExitOnWindowsFunctionFailureWithLastError(
+    const wchar_t* function_name,
+    DWORD last_error,
+    const wchar_t* file_name,
+    int line
+) {
+#if _DEBUG
+  wchar_t full_message[512];
+
+  swprintf(
+      full_message,
+      sizeof(full_message) / sizeof(full_message[0]),
+      kFunctionFailErrorFormat,
+      file_name,
+      line,
+      function_name,
+      last_error
+  );
+
+  wchar_t message_box_caption[128];
+  swprintf(message_box_caption, "%s Failed", 128, function_name);
+
+  MessageBoxW(
+      NULL,
+      full_message,
+      message_box_caption,
+      MB_OK | MB_ICONERROR
+  );
+#endif // _DEBUG
+
+  exit(EXIT_FAILURE);
+}
