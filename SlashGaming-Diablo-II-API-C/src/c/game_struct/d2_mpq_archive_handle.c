800ed51274fe8879bc5b5ff4df16c1f569616778
IAmTrial <IAmRealTrial@gmail.com>
2020-01-28 21:35:54 -0800

    Add MPQArchiveHandle struct

---
+++ SlashGaming-Diablo-II-API-C/src/c/game_struct/d2_mpq_archive_handle.c
@@ -0,0 +1,110 @@
+/**
+ * SlashGaming Diablo II Modding API for C
+ * Copyright (C) 2018-2020  Mir Drualga
+ *
+ * This file is part of SlashGaming Diablo II Modding API for C.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Additional permissions under GNU Affero General Public License version 3
+ *  section 7
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with Diablo II (or a modified version of that game and its
+ *  libraries), containing parts covered by the terms of Blizzard End User
+ *  License Agreement, the licensors of this Program grant you additional
+ *  permission to convey the resulting work. This additional permission is
+ *  also extended to any combination of expansions, mods, and remasters of
+ *  the game.
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with any Graphics Device Interface (GDI), DirectDraw, Direct3D,
+ *  Glide, OpenGL, or Rave wrapper (or modified versions of those
+ *  libraries), containing parts not covered by a compatible license, the
+ *  licensors of this Program grant you additional permission to convey the
+ *  resulting work.
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with any library (or a modified version of that library) that links
+ *  to Diablo II (or a modified version of that game and its libraries),
+ *  containing parts not covered by a compatible license, the licensors of
+ *  this Program grant you additional permission to convey the resulting
+ *  work.
+ */
+
+#include "../../../include/c/game_struct/d2_mpq_archive_handle.h"
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "../backend/error_handling.h"
+#include "../../wide_macro.h"
+
+/**
+ * Static assertions
+ */
+
+static_assert(
+    sizeof(struct D2_MPQArchiveHandle_1_00) == 264,
+    "Incorrect size."
+);
+
+static_assert(
+    offsetof(struct D2_MPQArchiveHandle_1_00, mpq_archive) == 0,
+    "Incorrect member alignment."
+);
+
+static_assert(
+    offsetof(struct D2_MPQArchiveHandle_1_00, mpq_archive_path) == 4,
+    "Incorrect member alignment."
+);
+
+/**
+ * Function definitions
+ */
+
+struct D2_MPQArchive* D2_MPQArchiveHandle_GetMPQArchive(
+    struct D2_MPQArchiveHandle* mpq_archive_handle
+) {
+  return (struct D2_MPQArchive*) D2_MPQArchiveHandle_GetConstMPQArchive(
+      mpq_archive_handle
+  );
+}
+
+const struct D2_MPQArchive* D2_MPQArchiveHandle_GetConstMPQArchive(
+    const struct D2_MPQArchiveHandle* mpq_archive_handle
+) {
+  const struct D2_MPQArchiveHandle_1_00* actual_mpq_archive_handle =
+      (const struct D2_MPQArchiveHandle_1_00*) mpq_archive_handle;
+
+  return (const struct D2_MPQArchive*) actual_mpq_archive_handle->mpq_archive;
+}
+
+char* D2_MPQArchiveHandle_GetMPQArchivePath(
+    struct D2_MPQArchiveHandle* mpq_archive_handle
+) {
+  return (char*) D2_MPQArchiveHandle_GetConstMPQArchivePath(
+      mpq_archive_handle
+  );
+}
+
+const char* D2_MPQArchiveHandle_GetConstMPQArchivePath(
+    const struct D2_MPQArchiveHandle* mpq_archive_handle
+) {
+  const struct D2_MPQArchiveHandle_1_00* actual_mpq_archive_handle =
+      (const struct D2_MPQArchiveHandle_1_00*) mpq_archive_handle;
+
+  return actual_mpq_archive_handle->mpq_archive_path;
+}
