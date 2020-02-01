5b20c79bf5ae31cd6ad0cf4d975fbe0b657e2390
IAmTrial <IAmRealTrial@gmail.com>
2020-02-01 12:48:56 -0800

    Add CelFile struct

---
+++ SlashGaming-Diablo-II-API-C/src/c/game_struct/d2_cel_file.c
@@ -0,0 +1,134 @@
+/**
+ * SlashGaming Diablo II Modding API for C
+ * Copyright (C) 2018-2020  Mir Drualga
+ *
+ * This file is part of SlashGaming Diablo II Modding API for C.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Additional permissions under GNU Affero General Public License version 3
+ *  section 7
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with Diablo II (or a modified version of that game and its
+ *  libraries), containing parts covered by the terms of Blizzard End User
+ *  License Agreement, the licensors of this Program grant you additional
+ *  permission to convey the resulting work. This additional permission is
+ *  also extended to any combination of expansions, mods, and remasters of
+ *  the game.
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with any Graphics Device Interface (GDI), DirectDraw, Direct3D,
+ *  Glide, OpenGL, or Rave wrapper (or modified versions of those
+ *  libraries), containing parts not covered by a compatible license, the
+ *  licensors of this Program grant you additional permission to convey the
+ *  resulting work.
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with any library (or a modified version of that library) that links
+ *  to Diablo II (or a modified version of that game and its libraries),
+ *  containing parts not covered by a compatible license, the licensors of
+ *  this Program grant you additional permission to convey the resulting
+ *  work.
+ */
+
+#include "../../../include/c/game_struct/d2_cel_file.h"
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "../backend/error_handling.h"
+#include "../../wide_macro.h"
+
+/**
+ * Static assertions
+ */
+
+static_assert(
+    sizeof(struct D2_CelFile_1_00) >= 0x18,
+    "Incorrect size."
+);
+
+static_assert(
+    offsetof(struct D2_CelFile_1_00, version) == 0x00,
+    "Incorrect member alignment."
+);
+
+static_assert(
+    offsetof(struct D2_CelFile_1_00, num_directions) == 0x10,
+    "Incorrect member alignment."
+);
+
+static_assert(
+    offsetof(struct D2_CelFile_1_00, num_frames) == 0x14,
+    "Incorrect member alignment."
+);
+
+static_assert(
+    offsetof(struct D2_CelFile_1_00, cels) == 0x18,
+    "Incorrect member alignment."
+);
+
+/**
+ * Function definitions
+ */
+
+unsigned int D2_CelFile_GetVersion(const struct D2_CelFile* cel_file) {
+  const struct D2_CelFile_1_00* actual_cel_file =
+      (const struct D2_CelFile_1_00*) cel_file;
+
+  return actual_cel_file->version;
+}
+
+void D2_CelFile_SetVersion(struct D2_CelFile* cel_file, unsigned int value) {
+  struct D2_CelFile_1_00* actual_cel_file =
+      (struct D2_CelFile_1_00*) cel_file;
+
+  actual_cel_file->version = value;
+}
+
+unsigned int D2_CelFile_GetNumDirections(const struct D2_CelFile* cel_file) {
+  const struct D2_CelFile_1_00* actual_cel_file =
+      (const struct D2_CelFile_1_00*) cel_file;
+
+  return actual_cel_file->num_directions;
+}
+
+void D2_CelFile_SetNumDirections(
+    struct D2_CelFile* cel_file,
+    unsigned int value
+) {
+  struct D2_CelFile_1_00* actual_cel_file =
+      (struct D2_CelFile_1_00*) cel_file;
+
+  actual_cel_file->num_directions = value;
+}
+
+unsigned int D2_CelFile_GetNumFrames(const struct D2_CelFile* cel_file) {
+  const struct D2_CelFile_1_00* actual_cel_file =
+      (const struct D2_CelFile_1_00*) cel_file;
+
+  return actual_cel_file->num_frames;
+}
+
+void D2_CelFile_SetNumFrames(
+    struct D2_CelFile* cel_file,
+    unsigned int value
+) {
+  struct D2_CelFile_1_00* actual_cel_file =
+      (struct D2_CelFile_1_00*) cel_file;
+
+  actual_cel_file->num_frames = value;
+}
