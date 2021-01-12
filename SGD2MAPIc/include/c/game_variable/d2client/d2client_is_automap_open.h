7e1f702b4f5e198243d1a76dfd0c00b29edafd16
IAmTrial <IAmRealTrial@gmail.com>
2021-01-11 17:49:48 -0800

    Refactor D2Client IsAutomapOpen variable

--- SGD2MAPIc/include/c/game_variable/d2client/d2client_is_automap_open.h
+++ SGD2MAPIc/include/c/game_variable/d2client/d2client_is_automap_open.h
@@ -46,8 +46,6 @@
 #ifndef SGD2MAPI_C_GAME_VARIABLE_D2CLIENT_D2CLIENT_IS_AUTOMAP_OPEN_H_
 #define SGD2MAPI_C_GAME_VARIABLE_D2CLIENT_D2CLIENT_IS_AUTOMAP_OPEN_H_
 
-#include <stdbool.h>
-
 #include "../../game_bool.h"
 
 #include "../../../dllexport_define.inc"
@@ -56,10 +54,12 @@
 extern "C" {
 #endif /* __cplusplus */
 
-DLLEXPORT bool D2_D2Client_GetIsAutomapOpen(void);
+DLLEXPORT int D2_D2Client_GetIsAutomapOpen(void);
+
 DLLEXPORT mapi_bool32 D2_D2Client_GetIsAutomapOpen_1_00(void);
 
-DLLEXPORT void D2_D2Client_SetIsAutomapOpen(bool is_automap_open);
+DLLEXPORT void D2_D2Client_SetIsAutomapOpen(int is_automap_open);
+
 DLLEXPORT void D2_D2Client_SetIsAutomapOpen_1_00(mapi_bool32 is_automap_open);
 
 #ifdef __cplusplus
