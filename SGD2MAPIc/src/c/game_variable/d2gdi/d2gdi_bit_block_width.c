fcf34724dd4cf6aa821574439524b9a4b1cb22e8
IAmTrial <IAmRealTrial@gmail.com>
2021-01-13 02:20:14 -0800

    Refactor D2GDI BitBlockWidth variable

--- SGD2MAPIc/src/c/game_variable/d2gdi/d2gdi_bit_block_width.c
+++ SGD2MAPIc/src/c/game_variable/d2gdi/d2gdi_bit_block_width.c
@@ -45,49 +45,54 @@
 
 #include "../../../../include/c/game_variable/d2gdi/d2gdi_bit_block_width.h"
 
-#include <pthread.h>
-#include <stdint.h>
-
-#include "../../../asm_x86_macro.h"
-#include "../../backend/error_handling.h"
-#include "../../backend/game_address_table.h"
+#include "../../../../include/c/default_game_library.h"
+#include "../../../../include/c/game_address.h"
 #include "../../../../include/c/game_version.h"
-#include "../../../wide_macro.h"
+#include "../../backend/game_address_table.h"
 
-static pthread_once_t once_flag = PTHREAD_ONCE_INIT;
-static const struct MAPI_GameAddress* game_address;
+static struct Mapi_GameAddress game_address;
 
 static void InitGameAddress(void) {
-  game_address = GetGameAddress(
-      "D2GDI.dll",
+  game_address = Mapi_GameAddressTable_GetFromLibrary(
+      D2_DefaultLibrary_kD2GDI,
       "BitBlockWidth"
   );
 }
 
+static void InitStatic(void) {
+  static int is_game_address_init = 0;
+
+  if (!is_game_address_init) {
+    InitGameAddress();
+
+    is_game_address_init = 1;
+  }
+}
+
+/**
+ * External
+ */
+
 int D2_D2GDI_GetBitBlockWidth(void) {
+  InitStatic();
+
   return D2_D2GDI_GetBitBlockWidth_1_00();
 }
 
 int32_t D2_D2GDI_GetBitBlockWidth_1_00(void) {
-  int once_return = pthread_once(&once_flag, &InitGameAddress);
+  InitStatic();
 
-  if (once_return != 0) {
-    ExitOnCallOnceFailure(__FILEW__, __LINE__);
-  }
-
-  return *(int32_t*) game_address->raw_address;
+  return *(int32_t*) game_address.raw_address;
 }
 
 void D2_D2GDI_SetBitBlockWidth(int width) {
+  InitStatic();
+
   D2_D2GDI_SetBitBlockWidth_1_00(width);
 }
 
 void D2_D2GDI_SetBitBlockWidth_1_00(int32_t width) {
-  int once_return = pthread_once(&once_flag, &InitGameAddress);
+  InitStatic();
 
-  if (once_return != 0) {
-    ExitOnCallOnceFailure(__FILEW__, __LINE__);
-  }
-
-  *(int32_t*) game_address->raw_address = width;
+  *(int32_t*) game_address.raw_address = width;
 }
