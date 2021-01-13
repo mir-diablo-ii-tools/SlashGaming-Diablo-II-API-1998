a6499b29fa8eb81f91c93b5e4900ab9a384a3017
IAmTrial <IAmRealTrial@gmail.com>
2021-01-13 02:06:22 -0800

    Refactor D2Direct3D DisplayWidth variable

--- SGD2MAPIc/include/c/game_variable/d2direct3d/d2direct3d_display_width.h
+++ SGD2MAPIc/include/c/game_variable/d2direct3d/d2direct3d_display_width.h
@@ -46,7 +46,7 @@
 #ifndef SGD2MAPI_C_GAME_VARIABLE_D2DIRECT3D_D2DIRECT3D_DISPLAY_WIDTH_H_
 #define SGD2MAPI_C_GAME_VARIABLE_D2DIRECT3D_D2DIRECT3D_DISPLAY_WIDTH_H_
 
-#include <stdint.h>
+#include <mdc/std/stdint.h>
 
 #include "../../../dllexport_define.inc"
 
@@ -55,9 +55,11 @@ extern "C" {
 #endif /* __cplusplus */
 
 DLLEXPORT int D2_D2Direct3D_GetDisplayWidth(void);
+
 DLLEXPORT int32_t D2_D2Direct3D_GetDisplayWidth_1_00(void);
 
 DLLEXPORT void D2_D2Direct3D_SetDisplayWidth(int width);
+
 DLLEXPORT void D2_D2Direct3D_SetDisplayWidth_1_00(int32_t width);
 
 #ifdef __cplusplus
