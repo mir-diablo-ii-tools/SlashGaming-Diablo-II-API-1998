da47092af154218e9b2262b7d9176af38dc1f0db
IAmTrial <IAmRealTrial@gmail.com>
2021-01-13 22:29:55 -0800

    Refactor D2Glide DisplayWidth variable

--- SGD2MAPIc/include/c/game_variable/d2glide/d2glide_display_width.h
+++ SGD2MAPIc/include/c/game_variable/d2glide/d2glide_display_width.h
@@ -46,7 +46,7 @@
 #ifndef SGD2MAPI_C_GAME_VARIABLE_D2GLIDE_D2GLIDE_DISPLAY_WIDTH_H_
 #define SGD2MAPI_C_GAME_VARIABLE_D2GLIDE_D2GLIDE_DISPLAY_WIDTH_H_
 
-#include <stdint.h>
+#include <mdc/std/stdint.h>
 
 #include "../../../dllexport_define.inc"
 
@@ -55,9 +55,11 @@ extern "C" {
 #endif /* __cplusplus */
 
 DLLEXPORT int D2_D2Glide_GetDisplayWidth(void);
+
 DLLEXPORT int32_t D2_D2Glide_GetDisplayWidth_1_00(void);
 
 DLLEXPORT void D2_D2Glide_SetDisplayWidth(int width);
+
 DLLEXPORT void D2_D2Glide_SetDisplayWidth_1_00(int32_t width);
 
 #ifdef __cplusplus
